76393abe8f479b4c8430db9ee59d88b1cf2af244
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-01 17:03:56 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/MenuLayer.hpp>
+class $modify(MyMenuLayer, MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init()) {
+			return false;
+		}
+
+		auto myButton = CCMenuItemSpriteExtra::create(
+			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+			this,
+			menu_selector(MyMenuLayer::onMyButton)
+		);
+
+		auto menu = this->getChildByID("bottom-menu");
+		menu->addChild(myButton);
+
+		myButton->setID("my-button"_spr);
+
+		menu->updateLayout();
+
+		return true;
+	}
+
+	void onMyButton(CCObject*) {
+		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+	}
+};
