9f1ef53e60486fc06262a4f07f03661be4c7d962
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-01 18:33:57 +0100

    sorry

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Geode/geode.hpp>
+#include "setFPS.h"
+
+using namespace geode::prelude;
+
+class $modify(MyMenuLayer, MenuLayer) {
+    bool init() {
+        if (MenuLayer::init()) return false;
+        CCLabelBMFont* fpsLabel = CCLabelBMFont::create("FPS: ", "bigFont.fnt");
+        this->addChild(fpsLabel);
+
+        this->scheuleUpdate();
+        return true;
+    }
+    void onMoreGames(CCObject*) {
+        setFPS(144);
+    }
+    void MyMenuLayer::update(float dt) {
+        int currentFPS = (int)(1.0f / dt);
+
+        char text[10];
+        sprintf(text, "FPS: %d", currentFPS);
+        fpsLabel->setString(text);
+    }
+}
