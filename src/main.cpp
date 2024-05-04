0161cd7d9a352ed4144b4ff4703640b0147c623c
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-04 16:11:15 +0100

    oh i hate you github

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,16 @@
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, MenuLayer) {
+class $modify(MenuLayer) {
     bool init() {
-        if (MenuLayer::init()) return false;
-        CCLabelBMFont* fpsLabel = CCLabelBMFont::create("FPS: ", "bigFont.fnt");
-        this->addChild(fpsLabel);
+        if (!MenuLayer::init()) return false;
+        auto label = Label::createWithBMFont("bigFont.fnt", "");
+        this->addChild(label);
 
         this->schedule([=](float dt) {
             int fps = CCDirector::sharedDirector()->getFrameRate();
             label->setString(StringUtils::format("FPS %d", fps));
         }, "update fps");
         return true;
-    }
-    void onMoreGames(CCObject*) {
-        setFPS(144);
-    }
-    void MyMenuLayer::update(float dt) {
-        int currentFPS = (int)(1.0f / dt);
-
-        char text[10];
-        sprintf(text, "FPS: %d", currentFPS);
-        fpsLabel->setString(text);
     }
 }
