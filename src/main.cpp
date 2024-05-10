e61ce2f72dcf90c2655570d1918257281f7bb8a4
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-10 23:32:29 +0100

    gf

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 using namespace geode::prelude;
 
-void onMoreGames(CCObject*) {
-    showAlert("Hi", "hello");
+class $modify(MenuLayer) {
+        void onMoreGames(CCObject*) {
+        showAlert("Hi", "hello");
+    }
 }
