bd42e432d553245ff8ffccbe5344922f6be43117
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-11 19:49:46 +0100

    wait a minute

--- src/main.mm
+++ src/main.mm
@@ -7,11 +7,6 @@ using namespace geode::prelude;
 
 class $modify(MenuLayer) {
     void onMoreGames(CCObject*) {
-        auto *iconPath = (Mod::get()->getResourcesDir() / "geode-logo.png").string();
-
-        NSString *iconPathNS = [NSString stringWithUTF8String:iconPathString];
-
-        setAppIcon(iconPathNS);
-
+        
     }
 };
