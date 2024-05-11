5d503325b5fc72e35438e27d8f3ca62dba5bfb97
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-11 15:19:25 +0100

    aaaa

--- src/mainobj.mm
+++ src/mainobj.mm
@@ -23,37 +23,27 @@ void showAlert(const char *title, const char *message) {
     [rootViewController presentViewController:alertController animated:YES completion:nil];
 }
 
-class ImagePickerDelegate : public NSObject<UIImagePickerControllerDelegate, UINavigationControllerDelegate> {
-public:
-    void imagePickerControllerDidCancel(UIImagePickerController *picker) override {
-        [picker dismissViewControllerAnimated:YES completion:nil];
-    }
 
-    void imagePickerController(UIImagePickerController *picker, NSDictionary<UIImagePickerControllerInfoKey, id> *info) override {
+
+void pickImage() {
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+
+
+    imagePicker.delegate = (id<UIImagePickerControllerDelegate, UINavigationControllerDelegate>)^(UIImagePickerController *picker, NSDictionary<UIImagePickerControllerInfoKey, id> *info) {
         NSString *mediaType = info[UIImagePickerControllerMediaType];
         if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
             UIImage *selectedImage = info[UIImagePickerControllerOriginalImage];
             NSURL *imageURL = info[UIImagePickerControllerImageURL];
             NSString *imageName = [imageURL lastPathComponent];
-
+            
             showAlert("Selected Image", [imageName UTF8String]);
         }
-
+        
         [picker dismissViewControllerAnimated:YES completion:nil];
-    }
-};
-
-// i despise objective c++
-
-void pickImage() {
-    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
-    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-
-    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-
-    // Create an instance of ImagePickerDelegate and assign it as the delegate of the image picker
-    ImagePickerDelegate *pickerDelegate = [ImagePickerDelegate new];
-    imagePicker.delegate = pickerDelegate;
-
+    };
+    
     [rootViewController presentViewController:imagePicker animated:YES completion:nil];
 }
