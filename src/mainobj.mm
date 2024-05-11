9d590a85c268857fde79b9abeb38e700ea33951b
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-11 15:43:22 +0100

    asdasdasdasd

--- src/mainobj.mm
+++ src/mainobj.mm
@@ -25,32 +25,42 @@ void showAlert(const char *title, const char *message) {
 
 
 
+// Custom delegate class for image picker
+@interface ImagePickerDelegate : NSObject <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@end
+
+@implementation ImagePickerDelegate
+
+// Delegate method called when an image is picked
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey, id> *)info {
+    NSString *mediaType = info[UIImagePickerControllerMediaType];
+    if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
+        UIImage *selectedImage = info[UIImagePickerControllerOriginalImage];
+        NSURL *imageURL = info[UIImagePickerControllerImageURL];
+        NSString *imageName = [imageURL lastPathComponent];
+        
+        showAlert("Selected Image", [imageName UTF8String]);
+    }
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+// Delegate method called when the image picker is canceled
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
+
 void pickImage() {
     UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
     imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     
+    // Create an instance of our custom delegate class
+    ImagePickerDelegate *imagePickerDelegate = [[ImagePickerDelegate alloc] init];
+    imagePicker.delegate = imagePickerDelegate;
+    
+    // Get the root view controller to present the image picker
     UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-    
-    // Define a block type for the delegate
-    typedef void (^ImagePickerCompletionBlock)(UIImagePickerController *picker, NSDictionary<UIImagePickerControllerInfoKey, id> *info);
-    
-    // Create the block for delegate
-    ImagePickerCompletionBlock completionBlock = ^(UIImagePickerController *picker, NSDictionary<UIImagePickerControllerInfoKey, id> *info) {
-        NSString *mediaType = info[UIImagePickerControllerMediaType];
-        if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
-            UIImage *selectedImage = info[UIImagePickerControllerOriginalImage];
-            NSURL *imageURL = info[UIImagePickerControllerImageURL];
-            NSString *imageName = [imageURL lastPathComponent];
-            
-            showAlert("Selected Image", [imageName UTF8String]);
-        }
-        
-        [picker dismissViewControllerAnimated:YES completion:nil];
-    };
-    
-    // Assign the block to the delegate
-    imagePicker.delegate = (id<UINavigationControllerDelegate, UIImagePickerControllerDelegate>)completionBlock;
-    
-    // Present the image picker
     [rootViewController presentViewController:imagePicker animated:YES completion:nil];
 }
