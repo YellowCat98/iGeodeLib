c0e36a8d6269666f9ef4722828939d11418be88d
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-12 11:52:49 +0100

    forgot args in iGeodeLib.hpp

--- src/iGeodeLib.hpp
+++ src/iGeodeLib.hpp
@@ -1,5 +1,6 @@
 #pragma once
+#include <functional>
 
 void showAlert(const char *title, const char *message, const char *Btn);
 void iOSVersion();
-void CShowAlert(const char *title, const char *message, const char *Btn);
+void (const char *title, const char *message, const char *Btn, std::function<void()> callback = nullptr, const char *Btn2 = nullptr, std::function<void()> callback2 = nullptr);
