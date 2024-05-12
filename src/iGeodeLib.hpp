496370339f8c200f2b273357833a3ceb4748e0d8
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-12 11:58:46 +0100

    aw man

--- src/iGeodeLib.hpp
+++ src/iGeodeLib.hpp
@@ -3,4 +3,4 @@
 
 void showAlert(const char *title, const char *message, const char *Btn);
 void iOSVersion();
-void (const char *title, const char *message, const char *Btn, std::function<void()> callback = nullptr, const char *Btn2 = nullptr, std::function<void()> callback2 = nullptr);
+void CShowAlert(const char *title, const char *message, const char *Btn, std::function<void()> callback = nullptr, const char *Btn2 = nullptr, std::function<void()> callback2 = nullptr);
