2189931ffe472100ad42a05e1b7336df3412236c
YellowCat98 <elcucuyisgay@gmail.com>
2024-05-14 23:00:36 +0100

    ummmm

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,16 @@
 #include <Geode/Geode.hpp>
 #include <Geode/Modify/MenuLayer.hpp>
 #include "iGeodeLib.hpp"
+#include <string>
 
 using namespace geode::prelude;
 
 class $modify(MenuLayer) {
     void onMoreGames(CCObject*) {
-        iGeodeLib::CShowAlert("Hello", "hi", "go away", nullptr, "hi", nullptr);
+        std::string iOSVersionStr = iGeodeLib::iOSVersion();
+
+        const char* iOSVersion = iOSVersionStr.c_str();
+
+        FLAlertLayer::create("Hello", ("you are running ios" + iOSVersion).c_str(), "idc")->show();
     }
 };
